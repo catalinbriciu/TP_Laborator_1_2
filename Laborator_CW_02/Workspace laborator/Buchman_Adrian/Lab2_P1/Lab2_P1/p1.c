1d9e861d55f77839189b46f2fb58c1e78bd033e1
hollowcowst-code <71733686+hollowcowst-code@users.noreply.github.com>
2021-02-22 20:58:13 +0200

    Solved Pr1 Homework

--- Laborator_CW_02/Workspace laborator/Buchman_Adrian/Lab2_P1/Lab2_P1/p1.c
+++ Laborator_CW_02/Workspace laborator/Buchman_Adrian/Lab2_P1/Lab2_P1/p1.c
@@ -2,11 +2,44 @@
 #include<stdlib.h>
 
 #pragma warning(disable:4996)
+short int NrElemente, ElementCautat;
+short int TablouCautare[250];
+
+void Citire(short int *TablouCautare, short int *NrElemente, short int *ElementCautat) {
+	
+	printf("Dati numarul de elemente din tablou: ");
+	scanf("%hi", NrElemente);
+
+	for (int Index = 0; Index < *NrElemente; Index++) {
+		scanf("%hi", &TablouCautare[Index]);
+	}
+
+	printf("Dati elementul cautat: ");
+	scanf("%hi", ElementCautat);
+
+}
+
+unsigned char CautareLiniara(short int *TablouCautare, short int ElementCautat) {
+
+	int Index = 1;
+
+	while (Index < NrElemente && ElementCautat != TablouCautare[Index]) {
+		Index++;
+	}
+
+	if (ElementCautat == TablouCautare[Index]) {
+		return Index;
+	} else {
+		return 0;
+	}
+}
+
+
 
 int main() {
 	
-	printf("Hello world!\n");
-
+	Citire(TablouCautare, &NrElemente, &ElementCautat);
+	printf("Elementul se afla pe pozitia %hi\n", CautareLiniara(TablouCautare, ElementCautat));
 
 	system("pause");
 	return 0;
