e8584a6d4e4d0f9bc2dc76e75a6723161a0ebf60
AnamariaAlehandra0108 <79400727+AnamariaAlehandra0108@users.noreply.github.com>
2021-02-23 14:18:22 +0200

    Update Lab2_P1.c

--- Laborator_CW_02/Workspace laborator/Bulzan Dan Alexandru/Lab2_P/Lab2_P1.c
+++ Laborator_CW_02/Workspace laborator/Bulzan Dan Alexandru/Lab2_P/Lab2_P1.c
@@ -1,12 +1,32 @@
-#include<stdio.h>
-#include<stdlib.h>
+#include <stdio.h>
+#include <stdlib.h>
 
+unsigned char CautareLiniara(short int *TablouCautare, short int ElementCautat)
+{
+	int contor, NumaruldeElemente;
+	printf("\nIntroduceti numarul de elemente:\n");
+	scanf("%d", &NumaruldeElemente);
+	TablouCautare = (short int *)malloc(NumaruldeElemente * sizeof(short int));
+	printf("\nIntroduceti elemntele: \n");
+	for (contor = 0; contor < NumaruldeElemente; contor++)
+		scanf("%d", &TablouCautare[contor]);
+	printf("\nIntrodu elementul cautat: ");
+	scanf("%d", &ElementCautat);
+	for (contor = 0; contor < NumaruldeElemente; contor++)
+		if (ElementCautat == TablouCautare[contor])
+		{
+			printf("\nElementul a fost gasit %d", contor + 1);
+			break;
+		}
+	if (contor >= NumaruldeElemente)
+		printf("\n Elementul nu exista.");
 
-
-int main(void) {
-
-
-	printf("\n Hello World!");
-
+}
+int main()
+{
+	short int ElementCautat;
+	short int *TablouCautare;
+	CautareLiniara(&TablouCautare, &ElementCautat);
 	system("pause");
+	return 0;
 }
