3bc1bf8a2aa5c80f3eb7535bc5af0aeab2d8f915
danalexandru27 <dan.bulzan@student.upt.ro>
2021-02-23 18:41:32 +0200

    tema:)

--- Laborator_CW_02/Workspace laborator/Bulzan Dan Alexandru/Lab2_P/Lab2_P1.c
+++ Laborator_CW_02/Workspace laborator/Bulzan Dan Alexandru/Lab2_P/Lab2_P1.c
@@ -1,32 +1,40 @@
-#include <stdio.h>
-#include <stdlib.h>
+#include<stdio.h>
+#include<stdlib.h>
 
-unsigned char CautareLiniara(short int *TablouCautare, short int ElementCautat)
-{
-	int contor, NumaruldeElemente;
-	printf("\nIntroduceti numarul de elemente:\n");
-	scanf("%d", &NumaruldeElemente);
-	TablouCautare = (short int *)malloc(NumaruldeElemente * sizeof(short int));
-	printf("\nIntroduceti elemntele: \n");
-	for (contor = 0; contor < NumaruldeElemente; contor++)
-		scanf("%d", &TablouCautare[contor]);
-	printf("\nIntrodu elementul cautat: ");
-	scanf("%d", &ElementCautat);
-	for (contor = 0; contor < NumaruldeElemente; contor++)
-		if (ElementCautat == TablouCautare[contor])
-		{
-			printf("\nElementul a fost gasit %d", contor + 1);
-			break;
-		}
-	if (contor >= NumaruldeElemente)
-		printf("\n Elementul nu exista.");
+unsigned char CautareLiniara(short int *a,short int*nr) {
+
+	short int contor = 0, x = 0;
+
+	printf("\n Numarul pe care doriti sa-l cautati: ");
+	scanf("%hd", &x);
+
+	for (contor;contor < *nr;contor++) {
+
+		printf("\n a[%d]= ", contor + 1);
+		scanf("%hd", (a+contor));
+
+	}
+	
+	for (contor=0;contor < *nr;contor++) {
+
+		if (*(a + contor) == x)return contor+1+'0';
+
+	}
+	
+	if (contor = *nr - 1)return 0+'0';
+
 
 }
-int main()
-{
-	short int ElementCautat;
-	short int *TablouCautare;
-	CautareLiniara(&TablouCautare, &ElementCautat);
+
+
+int main(void) {
+
+	short int *a, nrelemente;
+	printf("\n Dati numarul de elemente ale vectorului: ");
+	scanf("%hd", &nrelemente);
+	a = malloc(nrelemente * sizeof(short int));
+	printf("\n%c\n",CautareLiniara(a, &nrelemente));
+	free(a);
+
 	system("pause");
-	return 0;
 }
