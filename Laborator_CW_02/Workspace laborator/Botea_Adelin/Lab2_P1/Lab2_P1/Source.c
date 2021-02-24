33a3be3ec90adf320f4d19462f5ccfd15288397c
AdelinBotea <79401092+AdelinBotea@users.noreply.github.com>
2021-02-24 16:22:21 +0200

    .

---
+++ Laborator_CW_02/Workspace laborator/Botea_Adelin/Lab2_P1/Lab2_P1/Source.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+unsigned char CautareLiniara(short int *TablouCautare, unsigned char NumarElementeTablou, short int ElementCautat)
+{
+	unsigned char index;
+	for (index = 0; index < NumarElementeTablou; index++)
+		if (TablouCautare[index] == ElementCautat)
+			return index + 1;
+	return 0;
+}
+int main()
+{
+	short int TablouCautare[1000], ElementCautat;
+	unsigned char NumarElementeTablou, index;
+	printf("Introduceti numarul cautat:");
+	scanf("%d", &ElementCautat);
+	printf("Introduceti numarul de elemente al listei:");
+	scanf("%d", &NumarElementeTablou);
+	for (index = 0; index < NumarElementeTablou; index++)
+	{
+		printf("TablouCautare[%d]=", index);
+		scanf("%d", &TablouCautare[index]);
+	}
+	printf("%d\n", CautareLiniara(TablouCautare, NumarElementeTablou, ElementCautat));
+	system("pause");
+	return 0;
+}
