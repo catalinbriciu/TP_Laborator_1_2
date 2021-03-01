6751094255c843877e7c5150da28835a0fcff446
AdelinBotea <79401092+AdelinBotea@users.noreply.github.com>
2021-03-01 19:06:26 +0200

    .

---
+++ Laborator_CW_03/Workspace laborator/Botea_Adelin/Problema_lab3_tp/Problema_lab3_tp/Source.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#include <stdlib.h>
+unsigned char CautareLiniara(short int *TablouCautare, unsigned char NumarElementeTablou, short int ElementCautat)
+{
+	unsigned char index;
+	int li, ls, k;
+	ls = NumarElementeTablou;
+	li = 1;
+	while (li <= ls)
+	{
+		k = (li + ls) / 2;
+		if (ElementCautat == TablouCautare[k])
+			return k;
+		else
+			if (ElementCautat < TablouCautare[k])
+				ls = k - 1;
+			else li = k + 1;
+	}
+	return 0;
+}
+int main()
+{
+	short int TablouCautare[1000], ElementCautat;
+	unsigned char NumarElementeTablou, index;
+	printf("Introduceti numarul cautat:");
+	scanf("%d", &ElementCautat);
+	printf("Introduceti numarul de elemente al listei:");
+	scanf("%d", &NumarElementeTablou);
+	for (index = 0; index < NumarElementeTablou; index++)
+	{
+		printf("TablouCautare[%d]=", index);
+		scanf("%d", &TablouCautare[index]);
+	}
+	printf("%d\n", CautareLiniara(TablouCautare, NumarElementeTablou, ElementCautat));
+	system("pause");
+	return 0;
+}
