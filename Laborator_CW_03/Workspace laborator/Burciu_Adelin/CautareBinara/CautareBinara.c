8ce8f7453cde937e132a686c8263abfb17522dde
Adelin-Burciu <costinel.burciu@student.upt.ro>
2021-03-03 23:45:24 +0200

    ,

--- Laborator_CW_03/Workspace laborator/Burciu_Adelin/CautareBinara/CautareBinara.c
+++ Laborator_CW_03/Workspace laborator/Burciu_Adelin/CautareBinara/CautareBinara.c
@@ -3,27 +3,28 @@
 unsigned char CautareBinara(unsigned short int* tablou, unsigned short int element_cautat, unsigned short int nr_elemente) {
 
 	unsigned short int i = 0;
-	short int k, aux;
+	short int k, aux, limita_stanga, limita_dreapta, pivot;
+
+	limita_stanga = 1;
+	limita_dreapta = nr_elemente;
 	do {
-		k = 1;
-		for (i = 0; i < nr_elemente; i++)
-			if (tablou[i] > tablou[i + 1]) {
-				aux = tablou[i];
-				tablou[i] = tablou[i + 1];
-				tablou[i + 1] = aux;
-				k = 1;
-			}
 
-	} while (k);
+		pivot = (limita_stanga + limita_dreapta) / 2;
 
-	if (tablou[i] == element_cautat) {
-		return i;
-
-	}
-	else
-		return 0;
+		if (tablou[pivot] == element_cautat) {
+			return pivot ;
+		}
+		
 
 
+		if (tablou[pivot] > element_cautat)
+			limita_dreapta = pivot - 1;
+		else
+			limita_stanga = pivot + 1;
+
+		
+	} while (limita_stanga<=limita_dreapta);
+	return 0;
 }
 
 int main() {
@@ -31,7 +32,7 @@ int main() {
 	unsigned short int ElementCautat, nr_el;
 	printf("Dati nr de elemente:");
 	scanf("%hu", &nr_el);
-	for (unsigned short i = 0; i < nr_el; i++) {
+	for (unsigned short i = 1; i <= nr_el; i++) {
 		printf("tablou[%hu]=", i);
 		scanf("%hu", &tablou[i]);
 	}
@@ -39,7 +40,7 @@ int main() {
 	printf("Dati nr cautat:");
 	scanf("%hu", &ElementCautat);
 
-	if (CautareBinara(tablou, ElementCautat, nr_el) != 0)
+	if (CautareBinara(tablou, ElementCautat, nr_el) !=0)
 		printf("pozitia elementului cautat este:%hhu", CautareBinara(tablou, ElementCautat, nr_el));
 	else
 		printf("nu s-a gasit elementul cautat");
