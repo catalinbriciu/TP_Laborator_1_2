0828eb3ebe6ee544f63da1a07cd647e40cb89617
adrianchesa <paul.chesa@student.upt.ro>
2021-03-08 19:33:10 +0200

    flags1

---
+++ Laborator_CW_04/Workspace laborator/Chesa Paul-Adrian/Flags1/Flags1/Flag.c
@@ -0,0 +1,36 @@
+#include<stdio.h>
+unsigned short SetFlag(short int nr, unsigned char poz)
+{
+	return (nr | (1 << poz));
+	
+}
+
+const short int nr=15;
+const unsigned char poz=2;
+
+unsigned short ChangeFlag(short int nr, unsigned char poz)
+{
+	return (nr ^ (1 << poz));
+}
+unsigned short ClearFlag(short int nr, unsigned char poz)
+{
+	return(nr&(~(1 << poz)));
+}
+unsigned char GetFlag(short int nr, unsigned char poz) {
+	if ((nr & (1 << poz) == (1 << poz))) {
+		return 1;
+	}
+	else {
+		return 0;
+	}
+}
+int main()
+{
+	
+	SetFlag(nr,poz );
+	printf("%hu\n", SetFlag(nr,poz));
+	printf("%hu\n", ChangeFlag(nr, poz));
+	printf("%hu\n", ClearFlag(nr, poz));
+	printf("%hu\n", GetFlag(nr, poz));
+	return 0;
+}
