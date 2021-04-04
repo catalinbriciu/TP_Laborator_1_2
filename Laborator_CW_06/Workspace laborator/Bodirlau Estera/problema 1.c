7c4e5440d6e521518519a093d07a60a68d117737
Bodirlau Estera <79453559+estera15@users.noreply.github.com>
2021-04-04 22:19:35 +0300

    Tema

---
+++ Laborator_CW_06/Workspace laborator/Bodirlau Estera/problema 1.c
@@ -0,0 +1,15 @@
+#include<stdlib.h>
+#include<stdio.h>
+
+int v = 9;
+int *vPtr;
+
+int main()
+{
+	printf("%p\n", vPtr);
+	vPtr = &v;
+	printf("%d\n", *vPtr);
+	printf("%p\n", vPtr);
+	printf("%p\n", &v);
+	return 0;
+}
