efa410162006eb5f4e75f5dea325fb128d9aac3d
hollowcowst-code <adrian.buchman@student.upt.ro>
2021-04-12 18:58:56 +0300

    PermutariRecursiv

---
+++ Laborator_CW_09/Workspace laborator/Buchman_Adrian/PermutariRecursiv.c
@@ -0,0 +1,53 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+#pragma warning(disable:4996)
+
+unsigned char Stack[100];
+unsigned char PermutationNumber;
+
+unsigned char isValid(unsigned char StackPeak);
+unsigned char isSolution(unsigned char StackPeak);
+void Backtracking(unsigned char StackPeak);
+void PrintSolution(unsigned char StackPeak);
+
+int main() {
+	if (scanf("%hhu", &PermutationNumber)) {}
+	else {} //DUMMY if-else for return value of scanf
+	Backtracking(1);
+	system("pause");
+	return 0;
+}
+
+void Backtracking(unsigned char StackPeak) {
+	for (unsigned char Index = 1; Index <= PermutationNumber; Index++) {
+		Stack[StackPeak] = Index;
+		if (isValid(StackPeak)) {
+			if (isSolution(StackPeak)) {
+				PrintSolution(StackPeak);
+			} else {
+				Backtracking(StackPeak + 1);
+			}
+		}
+	}
+}
+
+unsigned char isValid(unsigned char StackPeak) {
+	for (unsigned char Index = 1; Index < StackPeak ; Index++) {
+		if (Stack[Index] == Stack[StackPeak]) {
+			return 0;
+		}
+	}
+	return 1;
+}
+
+unsigned char isSolution(unsigned char StackPeak) {
+	return StackPeak == PermutationNumber;
+}
+
+void PrintSolution(unsigned char StackPeak) {
+	for (unsigned char Index = 1; Index <= StackPeak; Index++) {
+		printf("%hhu ", Stack[Index]);
+	}
+	printf("\n");
+}
