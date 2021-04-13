5ec05d81d8ad7872c7e539a1975b6c3ecb2869ee
notRares22 <rares.bosoncea@student.upt.ro>
2021-04-13 16:26:34 +0300

    Create permutari.c

---
+++ Laborator_CW_09/Workspace laborator/Bosoncea Rares/permutari.c
@@ -0,0 +1,60 @@
+#include <stdlib.h>
+#include <stdio.h>
+int n, v[20];
+void init(int k)
+{
+	v[k] = 0;
+}
+int succesor(int k)
+{
+	if (v[k] < n)
+	{
+		v[k]++;
+		return 1;
+	}
+	else
+		return 0;
+}
+int valid(int k)
+{
+	int i;
+	for (i = 1; i < k; i++)
+		if (v[k] == v[i])
+			return 0;
+		else
+			return 1;
+}
+int solution(int k)
+{
+	return (k == n);
+}
+void afisare(int k)
+{
+	int i;
+	for (i = 1; i <= k; i++)
+		printf("%d ", v[i]);
+	printf("\n");
+}
+void BK(int k)
+{
+	int i;
+
+	for (i = 1; i <= n; i++)
+	{
+		v[k] = i;
+		if (valid(k))
+		{
+			if (solution(k))
+				afisare(k);
+			else
+				BK(k + 1);
+		}
+	}
+}
+
+int main()
+{
+	printf("n=");
+	scanf("%d", &n);
+	BK(1);
+}
