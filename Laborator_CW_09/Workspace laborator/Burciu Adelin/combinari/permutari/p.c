89d03e551f7b1e128998a9e1fb0f010180ca7cd3
Adelin-Burciu <79465588+Adelin-Burciu@users.noreply.github.com>
2021-04-16 22:27:33 +0300

    k

---
+++ Laborator_CW_09/Workspace laborator/Burciu Adelin/combinari/permutari/p.c
@@ -0,0 +1,52 @@
+#include<stdio.h>
+#define MAX 20
+int n, v[MAX]; 
+int valid(int k);
+int solutie(int k);
+void afisare(int k);
+void BK(int k);
+int main()
+{
+
+	printf("dati valoarea nr:");
+	scanf("%d", &n);
+	BK(1);
+	return 0; 
+}
+void BK(int k)
+{
+	int i; 
+
+	for (i = 1; i <= n; i++) 
+	{
+		v[k] = i; 
+		if (valid(k)) 
+		{
+			if (solutie(k)) 
+				afisare(k);
+			else
+				BK(k + 1); 
+		}
+	}
+}
+int valid(int k) 
+{
+	int i;
+	for (i = 1; i <= k - 1; i++) 
+		if (v[i] == v[k]) 
+			return 0; 
+	return 1; 
+}
+int solutie(int k) 
+{
+	if (k == n) 
+		return 1;
+	return 0;
+}
+void afisare(int k)
+{
+	int i;
+	for (i = 1; i <= k; i++)
+		printf("%d", v[i]);
+	printf("\n");
+}
