337b21953e44c3423994806ed3a7244bb00ee4ee
Ari Zeynep Melisa <melisaari81@gmail.com>
2021-04-26 19:16:21 +0300

    solutie

--- Primul_Test/Ari Zeynep Melisa/AriZeynepMelisa.c
+++ Primul_Test/Ari Zeynep Melisa/AriZeynepMelisa.c
@@ -5,11 +5,30 @@
 
 unsigned short int Stiva[MAX], VarfStiva;
 
-void Push(unsigned short int *Stiva, unsigned short int *VarfStiva, unsigned char Valoare) {
+void Push(unsigned short int Stiva[MAX], unsigned short int *VarfStiva, unsigned short int Valoare);
+unsigned short int Pop(unsigned short int Stiva[MAX], unsigned short int *VarfStiva);
+unsigned char IsStackEmpty(unsigned short int *VarfStiva);
+void creare_stiva(unsigned short int Stiva[MAX], unsigned short int *VarfStiva);
+void adaugare_stiva(unsigned short int Stiva[MAX], unsigned short int *VarfStiva);
+void parcurgere_stiva(unsigned short int Stiva[MAX], unsigned short int *VarfStiva);
+unsigned char cati_biti_de_1(unsigned short int numar);
+unsigned short int mascare_bit_2(unsigned short int numar);
+unsigned short int bit_6_devine_0(unsigned short int numar);
+
+int main() {
+	creare_stiva(Stiva, &VarfStiva);
+	adaugare_stiva(Stiva, &VarfStiva);
+	parcurgere_stiva(Stiva, &VarfStiva);
+
+	system("pause");
+	return 0;
+}
+
+void Push(unsigned short int Stiva[MAX], unsigned short int *VarfStiva, unsigned short int Valoare) {
 	Stiva[(*VarfStiva)++] = Valoare;
 }
 
-unsigned short int Pop(unsigned short int *Stiva, unsigned short int *VarfStiva) {
+unsigned short int Pop(unsigned short int Stiva[MAX], unsigned short int *VarfStiva) {
 	return Stiva[(*VarfStiva)--];
 }
 
@@ -22,7 +41,7 @@ unsigned char IsStackEmpty(unsigned short int *VarfStiva) {
 	}
 }
 
-void creare_stiva(unsigned short int *Stiva, unsigned short int *VarfStiva) {
+void creare_stiva(unsigned short int Stiva[MAX], unsigned short int *VarfStiva) {
 	unsigned char pozitie;
 	*VarfStiva = 0;
 	for (pozitie = 0; pozitie < MAX; pozitie++) {
@@ -30,7 +49,7 @@ void creare_stiva(unsigned short int *Stiva, unsigned short int *VarfStiva) {
 	}
 }
 
-void adaugare_stiva(unsigned short int *Stiva, unsigned short int *VarfStiva) {
+void adaugare_stiva(unsigned short int Stiva[MAX], unsigned short int *VarfStiva) {
 	Push(Stiva, &VarfStiva, 3);
 	Push(Stiva, &VarfStiva, 7);
 	Push(Stiva, &VarfStiva, 10);
@@ -40,12 +59,13 @@ void adaugare_stiva(unsigned short int *Stiva, unsigned short int *VarfStiva) {
 	Push(Stiva, &VarfStiva, 13);
 }
 
-void parcurgere_stiva(unsigned short int *Stiva, unsigned short int *VarfStiva) {
+void parcurgere_stiva(unsigned short int Stiva[MAX], unsigned short int *VarfStiva) {
 	unsigned short int numar;
 	while (!IsStackEmpty(&VarfStiva)) {
 		numar = Pop(Stiva, &VarfStiva);
 		printf("Numarul de biti de 1 al numarului %hu este: %huu\n", numar, cati_biti_de_1(numar));
-
+		printf("Numarul %hu dupa mascarea bitului 2 este: %hu\n", numar, mascare_bit_2(numar));
+		printf("Numarul %hu dupa ce bitul 6 devine 0 este: %hu\n", numar, bit_6_devine_0(numar));
 	}
 }
 
@@ -60,14 +80,9 @@ unsigned char cati_biti_de_1(unsigned short int numar) {
 }
 
 unsigned short int mascare_bit_2(unsigned short int numar) {
-	return (numar | (0 << 2));
+	return (numar & (~(1 << 2)));
 }
 
-int main() {
-	creare_stiva(Stiva, &VarfStiva);
-	adaugare_stiva(Stiva, &VarfStiva);
-	parcurgere_stiva(Stiva, &VarfStiva);
-
-	system("pause");
-	return 0;
+unsigned short int bit_6_devine_0(unsigned short int numar) {
+	return (numar & (~(1 << 6)));
 }
