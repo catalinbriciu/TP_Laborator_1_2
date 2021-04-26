ccb098d98843ccdb5431f2474fc02482356b5bc4
Miki <andreea.25oct@gmail.com>
2021-04-26 18:47:53 +0300

    Create TestTP.c

---
+++ Primul_Test/Botezatu Andreea/TestTP.c
@@ -0,0 +1,107 @@
+/*Sa se implementeze urmatoarea problema :
+a)  o stiva de maximum 10 elemente care contine numere intre 0 si 65000 (2.5 p)
+b) stiva trebuie implementata cel mai optim prin prisma consumului de memorie(1 p)
+c) se introduc in stiva 7 numere(1p)
+d) pt fiecare numar scos din stiva sa se afiseze(0.5 p)
+e) numarul de biti de 1 (2)
+f) numarul dupa mascarea bit - ului 2 (1 p)
+g) numarul dupa ce bitul 6 devine 0. (1p)*/
+
+#include<stdio.h>
+#include<stdlib.h>
+
+#define MAX 10
+unsigned short stack[MAX], top = -1;
+
+int isEmpty() {
+
+	if (top == -1)
+		return 1;
+	else
+		return 0;
+}
+
+int isFull() {
+
+	if (top == MAX)
+		return 1;
+	else
+		return 0;
+}
+
+
+int pop() {
+	unsigned short data;
+	if (!isEmpty()) {
+		data = stack[top];
+		top = top - 1;
+		return data;
+	}
+	else {
+		printf("Stiva goala.\n");
+	}
+}
+
+int push(unsigned short data) {
+
+	if (!isFull()) {
+		top = top + 1;
+		stack[top] = data;
+	}
+	else {
+		printf("Stiva plina.\n");
+	}
+}
+
+int bit_1(unsigned short Number)
+{
+	int i, k = 0;
+	for (i = 15; i >= 0; i--)
+	{
+		if (Number >> i & 1) k++;
+	}
+	return k;
+}
+
+unsigned int ClearFlag(unsigned short Number, unsigned short Position)
+{
+	Number = Number & ~(1 << Position);
+	return Number;
+}
+
+unsigned char GetFlag(unsigned short Number, unsigned short Position)
+{
+	Number = 1 & (Number >> Position);
+	return Number;
+}
+
+
+
+
+void main()
+{
+	int n, Nr, Biti1,Masc2,NrClear6, aux;
+	do {
+		printf("Dati cate numere doriti sa introduceti in stiva: ");
+		scanf("%d", &Nr);
+	} while(Nr > 10);
+
+	printf("Dati numerele ce urmeaza sa fie introduse in stiva: ");
+	do {
+		scanf("%d ", &n);
+		push(n);
+	} while (top != Nr-2);
+	
+	/*while (top != -1)
+	{
+		unsigned short x = pop();
+		Biti1 = bit_1(x);
+		Masc2 = ClearFlag(x, 2);
+		aux = GetFlag(x, 6);
+		if (aux == 1) {
+			ClearFlag(x, 6);
+		}
+		printf("Nr de biti de 1: %d\nDupa mascarea bitului 2: %d\nDupa ce bitul 6 devine 0: %d\n",Biti1,Masc2,x);
+	}*/
+
+}
