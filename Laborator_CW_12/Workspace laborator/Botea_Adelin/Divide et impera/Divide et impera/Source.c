4fe483850a19abb5a887cc046584f3a5ffdf0664
AdelinBotea <79401092+AdelinBotea@users.noreply.github.com>
2021-05-17 19:35:54 +0300

    .

--- Laborator_CW_12/Workspace laborator/Botea_Adelin/Divide et impera/Divide et impera/Source.c
+++ Laborator_CW_12/Workspace laborator/Botea_Adelin/Divide et impera/Divide et impera/Source.c
@@ -1,9 +1,10 @@
-#include <stdio.h>
-#include <stdlib.h>
-int DI(int li, int ls)
-{
+#include<stdio.h>
+#include<stdlib.h>
+
+unsigned char a[8] = { 5,2,6,10,9,1,7,8 };
+
+int DI(int li, int ls) {
 	int x, y;
-	int a[100];
 	if (li < ls) {
 		x = DI(li, (li + ls) / 2);
 		y = DI((li + ls) / 2 + 1, ls);
@@ -13,14 +14,11 @@ int DI(int li, int ls)
 	else
 		return a[li];
 }
+
 int main()
 {
-	int n, i, a[100];
-	printf("Nr-ul de elem:");
-	scanf("%d", &n);
-	for (i = 0; i < n; i++)
-		scanf("%d", &a[i]);
-	printf("%d ", DI(0, n - 1));
+	printf("%d\n", DI(0, 7));
+
 	system("pause");
 	return 0;
 }
