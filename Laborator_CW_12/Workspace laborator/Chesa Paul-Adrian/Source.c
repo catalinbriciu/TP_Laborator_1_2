ddcce790694d41794b128f12c13aa0aaa5f0e2c3
adrianchesa <paul.chesa@student.upt.ro>
2021-05-17 18:50:13 +0300

    Create Source.c

---
+++ Laborator_CW_12/Workspace laborator/Chesa Paul-Adrian/Source.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+unsigned char a[8] = { 5,2,6,10,9,1,7,8};
+
+int DI(int li, int ls) {
+	int x, y;
+	if (li < ls) {
+		x = DI(li, (li + ls) / 2);
+		y = DI((li + ls) / 2 + 1, ls);
+		if (x < y) return y;
+		else return x;
+	}
+	else
+		return a[li];
+}
+
+int main()
+{
+	printf("%d\n", DI(0, 7));
+
+	system("pause");
+	return 0;
+}
