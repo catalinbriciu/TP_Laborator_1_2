1474fdcf3149d494f39e82b8300a917159092d76
Andrei BUMBA <andrei.bumba@student.upt.ro>
2021-05-18 16:18:19 +0300

    tema

---
+++ Laborator_CW_12/Workspace laborator/Bumba Andrei/Iesire_din_labirint.c
@@ -0,0 +1,36 @@
+#include<stdio.h>
+#include<stdlib.h>
+int x[8] = { -1,-1,-1,0,1,1,1,0 };
+int y[8] = { -1,0,1,1,1,0,-1,-1 };
+int a[100][100] = { {0,0,0,1},{1,1,0,0},{1,1,0,0},{1,1,0,0},{1,0,0,0,0},{1,0,0,1},{1,1,0,1,0},{0,0,0,1} }, n = 4, m = 4, iesire;
+void Search(int i, int j) {
+	int k;
+	if (i == 0 || i == n - 1 || j == 0 || j == m - 1) {
+		iesire++;
+		printf("%d %d\n", i, j);
+	}
+	else
+		for (k = 0; k < 8; k++) {
+			if (a[i + x[k]][j + y[k]] == 0) {// se poate
+				a[i + x[k]][j + y[k]] = a[i][j] + 1;
+				Search(i + x[k], j + y[k]);
+				a[i + x[k]][j + y[k]] = 0; // !!!
+			}
+		}
+}
+int main()
+{
+	int i, j;
+	for (i = 0; i < n; i++)
+	{
+		for (j = 0; j < m; j++)
+			printf("%d ", a[i][j]);
+		printf("\n");
+	}
+	printf("\nIesiri:\n");
+	for (i = 0; i < n; i++)
+		for (j = 0; j < m; j++)
+			if (a[i][j] == 1)
+				Search(i, j);
+	printf("\nSunt %d posibilitati de a iesi din labirint!\n\n", iesire);
+}
---
+++ Laborator_CW_12/Workspace laborator/Bumba Andrei/Problema_fotografiei.c
@@ -0,0 +1,32 @@
+#include<stdio.h>
+#include<stdlib.h>
+int x[8] = { -1,-1,-1,0,1,1,1,0 };
+int y[8] = { -1,0,1,1,1,0,-1,-1 };
+int a[100][100] = { {0,0,0,1,1,0,1,0},{1,1,0,0,0,1,0,0},{1,1,0,1,0,1,0,0},{1,1,0,0,0,0,0,0},{1,1,0,0,0,0,0,0},{1,0,0,1,0,0,0,1},{1,1,1,1,0,0,1,0},{0,0,0,0,0,0,0,1} }, n = 8, nrObj = 1;
+void FillObj(int i, int j) { //marcare recursiva
+	int k; //local !
+	if (a[i][j] == 1) {
+		a[i][j] = nrObj; //numarul curent al obiectului
+		for (k = 0; k < 8; k++) //cele 8 directii
+			FillObj(i + x[k], j + y[k]);
+	}
+}
+int main()
+{
+	int i, j;
+	for (i = 0; i < n; i++)
+	{
+		for (j = 0; j < n; j++)
+			printf("%d ", a[i][j]);
+		printf("\n");
+	}
+	for (i = 0; i < n; i++)
+		for (j = 0; j < n; j++)
+			if (a[i][j] == 1)
+			{
+				nrObj++;
+				FillObj(i, j);
+			}
+	printf("\n\nIn fotografie sunt %d obiecte.\n\n", nrObj - 1);
+	return 0;
+}
